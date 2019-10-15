11078cea661ca285e9fcb4c42b1e02995fdb67df
Дмитрий Коваль <d.koval7@gmail.com>
2019-10-15 20:30:46 +0300

    build fix

--- dist/lib/http-interceptors/index.d.ts
+++ dist/lib/http-interceptors/index.d.ts
@@ -1,11 +1,11 @@
 import { ServerErrorInterceptor } from './server-error.interceptor';
 import { MessageInterceptor } from "./message.interceptor";
 export declare const ngCoreHttpInterceptorProviders: ({
-    provide: import("../../../../../../../../../../Users/user/projects/gfcafe/node_modules/@angular/core/src/di/injection_token").InjectionToken<import("../../../../../../../../../../Users/user/projects/gfcafe/node_modules/@angular/common/http/src/interceptor").HttpInterceptor[]>;
+    provide: import("@angular/core/src/di/injection_token").InjectionToken<import("@angular/common/http/src/interceptor").HttpInterceptor[]>;
     useClass: typeof ServerErrorInterceptor;
     multi: boolean;
 } | {
-    provide: import("../../../../../../../../../../Users/user/projects/gfcafe/node_modules/@angular/core/src/di/injection_token").InjectionToken<import("../../../../../../../../../../Users/user/projects/gfcafe/node_modules/@angular/common/http/src/interceptor").HttpInterceptor[]>;
+    provide: import("@angular/core/src/di/injection_token").InjectionToken<import("@angular/common/http/src/interceptor").HttpInterceptor[]>;
     useClass: typeof MessageInterceptor;
     multi: boolean;
 })[];
