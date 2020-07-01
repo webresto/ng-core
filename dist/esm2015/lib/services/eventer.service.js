b37f804a933394b9fe4775b53f33602c662cbeb2
Bozonych <bozonmarketolog@gmail.com>
2020-07-01 23:53:56 +0300

    check build

--- dist/esm2015/lib/services/eventer.service.js
+++ dist/esm2015/lib/services/eventer.service.js
@@ -1,6 +1,6 @@
 /**
  * @fileoverview added by tsickle
- * @suppress {checkTypes,extraRequire,missingOverride,missingReturn,unusedPrivateMembers,uselessCode} checked by tsc
+ * @suppress {checkTypes,extraRequire,missingReturn,uselessCode} checked by tsc
  */
 import { Injectable, EventEmitter } from '@angular/core';
 import * as i0 from "@angular/core";
