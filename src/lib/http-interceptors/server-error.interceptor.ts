c357e47beb059c2ba0fa4e0480dc5410bfd2ed57
larchenkov <Ivan.Larchenkov@pilot.ru>
2020-11-06 01:25:41 +0300

    логика упрщена, защита от некорректных значений

--- src/lib/http-interceptors/server-error.interceptor.ts
+++ src/lib/http-interceptors/server-error.interceptor.ts
@@ -27,18 +27,14 @@ export class ServerErrorInterceptor implements HttpInterceptor {
 
   intercept(req: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
     console.info('Interceptor', req);
-
     const authToken = localStorage.getItem(LS_TOKEN_NAME);
-
     return next.handle(!authToken ? req : req.clone({
       headers: req.headers.set('Authorization', `JWT ${authToken}`)
     })).pipe(
       tap(
         event => {
-          if (event instanceof HttpResponse) {
-            if (event.body.status && event.body.message && event.body.message[0]) {
-              throw new Error(event.body.message[0]);
-            }
+          if (event instanceof HttpResponse && event?.body?.status && event?.body?.message[0]) {
+              throw new Error(event?.body?.message[0]);
           }
         }
       ),
@@ -47,16 +43,15 @@ export class ServerErrorInterceptor implements HttpInterceptor {
   }
 
   private handleError(error: HttpErrorResponse) {
-
-    if (error.error.enable
-      && typeof error.error.title !== 'undefined'
-      && typeof error.error.description !== 'undefined'
-      && typeof error.error.startDate !== 'undefined'
-      && typeof error.error.stopDate !== 'undefined') {
+    if (error?.error?.enable
+      && typeof error?.error?.title !== 'undefined'
+      && typeof error?.error?.description !== 'undefined'
+      && typeof error?.error?.startDate !== 'undefined'
+      && typeof error?.error?.stopDate !== 'undefined') {
 
       const currentTime = new Date().getTime(),
-        startTime = new Date(error.error.startDate).getTime(),
-        stopTime = new Date(error.error.stopDate).getTime();
+        startTime = new Date(error?.error?.startDate).getTime(),
+        stopTime = new Date(error?.error?.stopDate).getTime();
 
       if (currentTime > startTime && currentTime < stopTime) {
         this.state.maintenance$.next({
@@ -64,52 +59,37 @@ export class ServerErrorInterceptor implements HttpInterceptor {
           description: error.error.description,
           social: error.error.social
         });
-      }
+      };
 
       return throwError(error.error);
-    }
-
-    if (error.error instanceof ErrorEvent) {
-      // A client-side or network error occurred. Handle it accordingly.
-      console.error('An error occurred:', error.error.message);
-    } else if (error instanceof Error) {
-      // A client-side or network error occurred. Handle it accordingly.
-      console.error('An error occurred:', error.message);
-
-      switch (error.message) {
-        case 'timeout-or-duplicate':
-          return throwError('Ошибка сервера (таймаут). Повторите попытку позже');
-      }
-    } else {
-      // The backend returned an unsuccessful response code.
-      // The response body may contain clues as to what went wrong,
-      console.error(
-        `Backend returned code ${error.status}, ` +
-        `body was: ${error.error}`);
-
-      if (error.status == 401) {
-        this.eventer.emitMessageEvent(
-          new EventMessage('Unauthorized', '', '')
-        );
-        localStorage.removeItem(LS_TOKEN_NAME);
-        return throwError(
-          error.error && error.error.title
-            ? error.error.title
-            : 'Необходимо пройти авторизацию'
-        );
-      } else if (error?.status == 404 && error?.error == "User not found") {
-        localStorage.removeItem(LS_TOKEN_NAME);
-      } else if ((error.status == 400 || error.status == 500)
-        && error.error
-        && error.error.message
-        && error.error.message.title
-        && error.error.message.body) {
-        this.eventer.emitMessageEvent(
-          new EventMessage('error', error.error.message.title, error.error.message.body)
-        );
-      }
-    }
+    };
+    switch (true) {
+      case error?.error instanceof ErrorEvent: console.error('An error occurred:', error?.error?.message); return throwError(error?.error);;
+      case (error instanceof Error && error?.message == 'timeout-or-duplicate'):
+        console.error('An error occurred:', error?.message);
+        return throwError('Ошибка сервера (таймаут). Повторите попытку позже');
+      case (error instanceof Error && error?.message != 'timeout-or-duplicate'):
+        console.error(
+          `Backend returned code ${error?.status}, ` + `body was: ${error?.error}`);
+        if (error?.status == 401) {
+          this.eventer.emitMessageEvent(
+            new EventMessage('Unauthorized', '', '')
+          );
+          localStorage.removeItem(LS_TOKEN_NAME);
+          return throwError(
+            error?.error?.title ? error?.error?.title : 'Необходимо пройти авторизацию'
+          );
+        } else if (error?.status == 404 && error?.error == "User not found") {
+          localStorage.removeItem(LS_TOKEN_NAME);
+        } else if ((error?.status == 400 || error?.status == 500)
+          && error?.error?.message?.title
+          && error?.error?.message?.body) {
+          this.eventer.emitMessageEvent(
+            new EventMessage('error', error?.error?.message?.title, error?.error?.message?.body)
+          );
+        }
+        return throwError(error?.error);
+    };
     // return an observable with a user-facing error message
-    return throwError(error.error);
   };
 }
